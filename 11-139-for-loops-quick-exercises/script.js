3460294de12219ae051b35ba60c55f7e1e8f4743
Ryan Bayse <ryanb@eml.cc>
2019-07-19 22:08:27 -0500

    for loops quick exercises successfully completed

---
+++ 11-139-for-loops-quick-exercises/script.js
@@ -0,0 +1,22 @@
+// For Loops Recipe
+// for (init; condition; step) {
+//     //run some code
+// }
+
+// For Loops Example
+// for (count = 0; count < 6; count++) {
+//     //console.log(count);
+// }
+
+// For Loops Exercise 1
+// for (var i = 0; i < 16; i+=8) {
+//     //console.log(i);
+// }
+// Answer: will print 0 then 8 in console
+
+// For Loops Exercise 2
+// var str = "ahceclwlxo"
+// for (var i = 1; i < str.length; i+=2) {
+//     //console.log(str[i]);
+// }
+// Answer: will print h, e, l, l, o (hello) in console
