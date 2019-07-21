f88c4c156803f340089aa31802c8ff05c9c2da2c
Ryan Bayse <ryanb@eml.cc>
2019-07-21 16:16:18 -0500

    for loops problem set successfully completed

--- 11-140-for-loops-problem-set/script.js
+++ 11-140-for-loops-problem-set/script.js
@@ -1,48 +1,33 @@
 // Print all numbers between -10 and 19
 console.log("Print all numbers between -10 and 19")
-var num = -10;
-
-while(num <= 19) {
+for (var num = 0; num <= 19; num++) {
     console.log(num);
-        num++;
 }
 
 // Print all even numbers between 10 and 40
 console.log("Print all even numbers between 10 and 40")
-var num = 10;
-
-while(num <= 40) {
+for (var num = 10; num <= 40; num+=2) {
     console.log(num);
-        num +=2;
 }
 
 // Print all odd numbers between 300 and 333 --Ryan's solution
 console.log("Print all odd numbers between 300 and 333 (Ryan's solution)")
-var num = 300;
-
-while(num <= 333) {
+for (var num = 300; num <= 333; num+=2) {
     console.log(num+1);
-        num +=2;
 }
 
 // Print all odd numbers between 300 and 333 --Colt's solution
 console.log("Print all odd numbers between 300 and 333 (Colt's solution)")
-var num = 300;
-
-while(num <= 333) {
+for (var num = 300; num <= 333; num+=1) {
     if(num % 2 !== 0) {
         console.log(num);
     }
-        num +=1;
 }
 
 // Print all numbers divisible by 5 AND 3 between 5 and 50
 console.log("Print all numbers divisible by 5 AND 3 between 5 and 50")
-var num = 5;
-
-while(num <= 50) {
+for (var num = 5; num <= 50; num++) {
     if((num % 5 === 0) && (num % 3 === 0)) {
         console.log(num);
     }
-    num++
 }
